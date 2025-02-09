518f28c941bcee79149341863566f7b33209fdd7
aki05162525 <akihiro05162525@gmail.com>
2025-02-09 22:48:33 +0900

    アイコンを追加

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHouse,
+  faFilePen,
+  faArrowRightToBracket,
+} from "@fortawesome/free-solid-svg-icons";
 
 function Navbar() {
   return (
     <nav>
-      <Link to="/">ホーム</Link>
-      <Link to="/createPost">記事投稿</Link>
-      <Link to="/Login">ログイン</Link>
+      <Link to="/">
+        <FontAwesomeIcon icon={faHouse} />
+        ホーム
+      </Link>
+      <Link to="/createPost">
+        <FontAwesomeIcon icon={faFilePen} />
+        記事投稿
+      </Link>
+      <Link to="/Login">
+        <FontAwesomeIcon icon={faArrowRightToBracket} />
+        ログイン
+      </Link>
     </nav>
   );
 }

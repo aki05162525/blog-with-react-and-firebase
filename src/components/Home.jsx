8de2e82609ec78ca608e1c01d3a42cfe15c2ccd7
aki05162525 <akihiro05162525@gmail.com>
2025-02-11 13:49:33 +0900

    homeコンポーネントを作成

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 function Home() {
-  return <div className="homepage"></div>;
+  return (
+    <div className="homepage">
+      <div className="postContents">
+        <div className="poseHeader">
+          <h1>タイトル</h1>
+        </div>
+      </div>
+      <div className="postTextContainer">
+        今はReactの学習中です。これから頑張ってReactエンジニアとしてヵつやくしていきたいと思っています。よろしくお願いします。
+      </div>
+      <div className="nameAndDeleteButton">
+        <h3>@akihiro</h3>
+        <button>削除</button>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
